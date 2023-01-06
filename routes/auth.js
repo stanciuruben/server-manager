0b3bd0ed5532eea912ad14506770d6200f9ac93d
Ruben Stanciu <stanciu.ruben20@gmail.com>
2023-01-06 13:16:19 +0100

    /auth route updated

--- routes/auth.js
+++ routes/auth.js
@@ -7,22 +7,22 @@ const escapeChars = require('../services/escapeChars');
 const serverResponses = require('../messages/resoponses.json');
 
 // @route   POST
-// @desc    Get credentials and return Token
+// @desc    Get credentials and return token
 // @access  Public
 router.post('/', async (req, res) => {
    const resLanguage = req.body.lang ?? 'en';
    try {
       const reqUsername = escapeChars(req.body.username);
       const reqPassword = escapeChars(req.body.password);
-      const usernameHash = config.get(escapeChars(req.body.client) + '.username');
-      const passwordHash = config.get(escapeChars(req.body.client) + '.password');
+      const usernameHash = config.get(escapeChars(req.body.client) + '.username') || '';
+      const passwordHash = config.get(escapeChars(req.body.client) + '.password') || '';
       const isUsernameValid = bcrypt.compareSync(reqUsername, usernameHash);
       const isPasswordValid = bcrypt.compareSync(reqPassword, passwordHash);
       if (isUsernameValid && isPasswordValid) {
          const token = await jwt.sign(
-            { admin: true },
+            { client: req.body.client, lang: req.body.lang, user: req.body.username },
             config.get('jwtSecret'),
-            { expiresIn: 60 * 60 * 5 } // 5 hours
+            { expiresIn: '1h' }
          );
          res.status(200).json({
             status: 200,
